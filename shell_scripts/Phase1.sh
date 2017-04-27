2d64bf82659c0fef74a45aa1d5160db697b7b34f
lazylad91 <parteek_makkar@ymail.com>
2017-04-27 02:51:02 -0700

    Phase 1 script

--- shell_scripts/Phase1.sh
+++ shell_scripts/Phase1.sh
@@ -3,23 +3,25 @@
 #!receive a path as a argument and process the image and put the result files back on the given path
 
 echo "image file is at: $1"
-export DEEPMASK="/home/cuda/cleanstreets/deepmask/deepmask"
-export DEEPMASKMODEL="/home/cuda/cleanstreets/deepmask/deepmask/pretrained/deepmask"
+export DEEPMASK="/home/cuda/deepmask"
+export DEEPMASKMODEL="/home/cuda/deepmask/customModel/sharpmask"
 
 pwd=`pwd`
 
 echo "starting deepmask and generating 15 proposal images"
 cd $DEEPMASK
-th computeProposals.lua $DEEPMASKMODEL  -img $1 -np 15
+th computeProposals.lua $DEEPMASKMODEL -img $1 -np 20 -gpu 2 -ss 0.8 -si -3.5 -sf 0.1
 
 echo "finished deepmask... now blending the proposals"
-python blendImages.py 15
+python blendImages.py 20
 
 echo "finally ... moving the file"
 mv $DEEPMASK/deepmask_output.jpg ~/clean_street_sink/temp
 
 echo "returning boundarybox as string"
-> result.json
+
+echo '{}' > result.json
+
 mv $DEEPMASK/result.json ~/clean_street_sink/temp
 
 
